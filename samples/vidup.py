2e02995e4f4243de6b7ffd51e4ce7d3b2820b24d
smy778 <dev@enc-dec.app>
2026-07-12 16:22:45 -0700

    vidup: add new source

---
+++ samples/vidup.py
@@ -0,0 +1,75 @@
+import requests
+import re
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36",
+    "Referer": "https://vidup.to/",
+    "X-Requested-With": "XMLHttpRequest"
+}
+
+API = "https://enc-dec.app/api"
+
+def validate(data, path):
+    if data["status"] != 200:
+        print(f"\n{'-'*25} API ERROR {'-'*25}\n")
+        print(f"Path: {path}")
+        print(f"Status Code: {data['status']}")
+        print(f"Error: {data.get('error', 'unknown')}")
+        raise SystemExit
+    return data["result"]
+
+# Movie format: <https://vidup.to/movie/{IMDB_ID or TMDB_ID}>
+# Tv format: <https://vidup.to/tv/{IMDB_ID or TMDB_ID}/{season_number}/{episode_number}>
+
+# --- Game of Thrones ---
+title = "Game of Thrones"
+type = "tv"
+year = "2011"
+imdb_id = "tt0944947"
+tmdb_id = "1399"
+season = "1"
+episode = "1"
+
+# Fetch page content
+base_url = f"https://vidup.to/tv/{tmdb_id}/{season}/{episode}/"
+response = requests.get(base_url).text
+
+# Extract text
+match = re.search(r'\\"en\\":\\"(.*?)\\"', response)
+text = match.group(1)
+
+# Get vidup urls
+enc_vidup = f"{API}/enc-vidup?text={text}"
+response = requests.get(enc_vidup).json()
+parts = validate(response, enc_vidup)
+servers = parts['servers']
+stream = parts['stream']
+token = parts['token']
+
+# Update headers with token
+HEADERS["X-CSRF-Token"] = token
+
+# Get streaming servers and decrypt
+servers_encrypted = requests.post(servers, headers=HEADERS).text
+
+dec_vidup = f"{API}/dec-vidup"
+response = requests.post(dec_vidup, json={"text": servers_encrypted}).json()
+servers_decrypted = validate(response, dec_vidup)
+
+# Sample the first server
+# Note: there are multiple server options in servers_decrypted, create the stream urls with different 'data' values.
+# For reference, run: print(servers_decrypted)
+server = servers_decrypted[0]
+data = server['data']
+
+# Get stream and decrypt
+stream = f"{stream}/{data}"
+stream_encrypted = requests.post(stream, headers=HEADERS).text
+
+dec_vidup = f"{API}/dec-vidup"
+response = requests.post(dec_vidup, json={"text": stream_encrypted}).json()
+stream_decrypted = validate(response, dec_vidup)
+
+print(f"\n{'-'*25} Decrypted Data {'-'*25}\n")
+print(f"Referer: {HEADERS['Referer']}\n")
+print(stream_decrypted)
