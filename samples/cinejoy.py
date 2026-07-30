f87fa507ef3cdcbca82a8a40c6984b9b71676b82
smy778 <dev@enc-dec.app>
2026-07-29 22:50:14 -0700

    videasy: update domain for latest | cinejoy: fix for latest

--- samples/cinejoy.py
+++ samples/cinejoy.py
@@ -37,10 +37,11 @@ def count_leading_zero_bits(data):
     return count
 
 def solver(data):
-    salt = bytes.fromhex(data["s"])
+    salt = hashlib.sha256(f'pow2-salt|{data["s"]}|{data["b"]}'.encode()).digest()
+    counter = 0
 
-    for counter in range(2**63):
-        payload = f'{data["b"]}:{data["s"]}:{counter}'.encode()
+    while True:
+        payload = f'pow2|{data["b"]}|{data["s"]}|{counter}'.encode()
 
         result = hashlib.scrypt(
             payload,
@@ -48,23 +49,26 @@ def solver(data):
             n=data["n"],
             r=data["r"],
             p=data["p"],
-            dklen=32,
+            dklen=32
         )
 
         if count_leading_zero_bits(result) >= data["d"]:
-            return base64.b64encode(json.dumps({**data, "c": counter}, separators=(",", ":")).encode()).decode()
+            response = json.dumps({**data, "c": counter}, separators=(",", ":")).encode()
+            return base64.b64encode(response).decode()
+
+        counter += 1
 
 def solve_challenge(rid):
-    url = f"https://api.shegu.xyz/challenge?rid={rid}"
+    url = f"https://api.shegu.st/challenge?rid={rid}"
     response = requests.get(url, headers=HEADERS)
     challenge = response.json()
 
     return solver(challenge)
 
-# Note that there are different servers, find them here: https://api.shegu.xyz/servers
+# Note that there are different servers, find them here: https://api.shegu.st/servers
 
-# Movie format: <https://api.shegu.xyz/?title={title}&type=movie&year={year}&imdb={imdb_id}&tmdb={tmdb_id}&server={server}>
-# Tv format: <https://api.shegu.xyz/?title={title}&type=series&year={year}&imdb={imdb_id}&tmdb={tmdb_id}&server={server}&season={season_number}&episode={episode_number}>
+# Movie format: <https://api.shegu.st/?title={title}&type=movie&year={year}&imdb={imdb_id}&tmdb={tmdb_id}&server={server}>
+# Tv format: <https://api.shegu.st/?title={title}&type=series&year={year}&imdb={imdb_id}&tmdb={tmdb_id}&server={server}&season={season_number}&episode={episode_number}>
 
 # --- Game of Thrones ---
 title = "Game of Thrones"
@@ -78,11 +82,11 @@ episode = "1"
 # Get sample server
 # Note: there are multiple server options, you can change the server by changing the index
 # For reference, run: print(servers)
-servers = requests.get("https://api.shegu.xyz/servers", headers=HEADERS).json()['servers']
+servers = requests.get("https://api.shegu.st/servers", headers=HEADERS).json()['servers']
 server = servers[0]['name']
 
 # Get encrypted text
-url = f"https://api.shegu.xyz/?title={quote(title)}&type={type}&year={year}&imdb={imdb_id}&tmdb={tmdb_id}&server={server}&season={season}&episode={episode}"
+url = f"https://api.shegu.st/?title={quote(title)}&type={type}&year={year}&imdb={imdb_id}&tmdb={tmdb_id}&server={server}&season={season}&episode={episode}"
 
 enc_cinejoy = f"{API}/enc-cinejoy?url={quote(url)}"
 response = requests.get(enc_cinejoy).json()
@@ -92,7 +96,7 @@ enc = validate(response, enc_cinejoy)
 HEADERS["x-at"] = solve_challenge(enc)
 
 # Get encrypted media data
-encrypted = requests.get(f"https://api.shegu.xyz/{enc}", headers=HEADERS).text
+encrypted = requests.get(f"https://api.shegu.st/{enc}", headers=HEADERS).text
 
 # Decrypt
 dec_cinejoy = f"{API}/dec-cinejoy"
